03f0698d272ecf093f3d88f53db2bc8095ac4dd8
lgorich <40358727+lgorich@users.noreply.github.com>
2021-03-29 18:01:08 +0300

    MASTER-1: added Car class, task 2

--- src/homework2/task2/Car.java
+++ src/homework2/task2/Car.java
@@ -1,4 +1,21 @@
 package homework2.task2;
 
 public class Car {
+    private int year;
+    private String colour;
+
+    public Car() {
+        this.year = 2014;
+        this.colour = "Black";
+    }
+
+    public Car(String colour) {
+        this.year = 2014;
+        this.colour = colour;
+    }
+
+    public Car(int year, String colour) {
+        this.year = year;
+        this.colour = colour;
+    }
 }
