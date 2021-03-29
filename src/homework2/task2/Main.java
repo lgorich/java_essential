3588cb12731dc498138725c81cc31caa283d3432
lgorich <40358727+lgorich@users.noreply.github.com>
2021-03-29 18:23:17 +0300

    MASTER-2: init Car from Main, task 2

--- src/homework2/task2/Main.java
+++ src/homework2/task2/Main.java
@@ -2,6 +2,10 @@ package homework2.task2;
 
 public class Main {
     public static void main(String[] args) {
+        Car carDefault = new Car();
 
+        Car carWithColour = new Car("Silver");
+
+        Car carWithYearClour = new Car(2021, "Yellow");
     }
 }
