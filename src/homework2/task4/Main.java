0ba2a82d5ef56b5afb88e49a118f241a8e7aba26
lgorich <40358727+lgorich@users.noreply.github.com>
2021-03-29 22:59:39 +0300

    MASTER-6: init Car from Main, task 4

--- src/homework2/task4/Main.java
+++ src/homework2/task4/Main.java
@@ -2,6 +2,14 @@ package homework2.task4;
 
 public class Main {
     public static void main(String[] args) {
+        Car carDefault = new Car();
 
+        Car car1Param = new Car("Silver");
+
+        Car car2Params = new Car(2021, "Yellow");
+        
+        Car car3Params = new Car(2021, 210.5, "Yellow");
+        
+        Car car4Params = new Car(2021, 210.5, 1500, "Yellow");
     }
 }
